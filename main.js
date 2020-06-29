13b51fe53d9f47b41ce682b101c0df28888c4f1e
Jesse <32210049+jessecookedesign@users.noreply.github.com>
2020-06-29 01:32:39 -0500

    Moved vue stuff to a new main.js file

---
+++ main.js
@@ -0,0 +1,63 @@
+var app = new Vue({
+            el: '#app',
+            data: {
+                cardData: [
+                    {
+                    imgsrc: "https://i.ibb.co/Lx9PLPx/ph-logo-mark-white-space.jpg",
+                    imgalt: "Pixl Haus logo",
+                    name: "Pixl Haus",
+                    category: "web design",
+                    address: "702 E 3rd St, Duluth, MN 55805",
+                    addresslink: "http://maps.google.com/?q=702 e 3rd st duluth mn 55805", website: "example.com",
+                    websitelink: "https://www.google.com",
+                    phone: "218-555-1234",
+                    phonelink: "tel: 1-218-555-5555",
+                    tag1: "freelancer",
+                    tag2: "creative"
+                    },
+                    {
+                    imgsrc: "https://i.ibb.co/fqs783v/dspclogo.png",
+                    imgalt: "Duluth Screen Printing Co logo",
+                    name: "Duluth Screen Printing Co.",
+                    category: "printing",
+                    address: "702 E 3rd St, Duluth, MN 55805",
+                    addresslink: "http://maps.google.com/?q=702 e 3rd st duluth mn 55805",
+                    website: "example.com",
+                    websitelink: "https://www.google.com",
+                    phone: "218-555-1234",
+                    phonelink: "tel: 1-218-555-5555",
+                    tag1: "affordable",
+                    tag2: "eco-friendly"
+                    },
+                    {
+                    imgsrc: "https://i.ibb.co/SDg1gcW/yourlogohere.jpg",
+                    imgalt: "Your logo here",
+                    name: "Business Name",
+                    category: "category here",
+                    address: "702 E 3rd St, Duluth, MN 55805",
+                    addresslink: "http://maps.google.com/?q=702 e 3rd st duluth mn 55805",
+                    website: "example.com",
+                    websitelink: "https://www.google.com",
+                    phone: "218-555-1234",
+                    phonelink: "tel: 1-218-555-5555",
+                    tag1: "tag one",
+                    tag2: "tag two"
+                    }
+                    ,
+                    {
+                    imgsrc: "https://i.ibb.co/SDg1gcW/yourlogohere.jpg",
+                    imgalt: "Your logo here",
+                    name: "Business Name",
+                    category: "category here",
+                    address: "702 E 3rd St, Duluth, MN 55805",
+                    addresslink: "http://maps.google.com/?q=702 e 3rd st duluth mn 55805",
+                    website: "example.com",
+                    websitelink: "https://www.google.com",
+                    phone: "218-555-1234",
+                    phonelink: "tel: 1-218-555-5555",
+                    tag1: "tag one",
+                    tag2: "tag two"
+                    }
+                ]
+            }
+        })
